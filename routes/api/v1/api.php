83e2cf8b23033273d5a67c6aa0b936a85e0bc336
MrMohebi <MMMohebi@outlook.com>
2021-05-09 16:39:10 +0430

    add api rout v1

---
+++ routes/api/v1/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+
+Route::prefix("/cuki")->group(function (){
+
+});
+
+Route::prefix("/res")->group(function (){
+
+});
+
+Route::prefix("/admin")->group(function (){
+
+});
+
+Route::prefix("/qr")->group(function (){
+
+});
+
+Route::prefix("/pay")->group(function (){
+
+});
+
