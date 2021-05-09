2fd4882c073de4459e04e8418b1988a459350be2
MrMohebi <MMMohebi@outlook.com>
2021-05-09 16:39:39 +0430

    create first controllers

---
+++ app/Http/Controllers/api/admin/v1/login.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\api\admin\v1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class login extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/api/cuki/v1/resData.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\api\cuki\v1;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+
+class resData extends Controller
+{
+    public function getResData(){
+
+    }
+}
