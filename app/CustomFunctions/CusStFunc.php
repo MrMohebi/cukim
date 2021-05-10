3819b5712740d98598ddcb10a201b81835662879
MrMohebi <MMMohebi@outlook.com>
2021-05-10 20:08:19 +0430

    create custom functions

---
+++ app/CustomFunctions/CusStFunc.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace App\CustomFunctions;
+
+
+class CusStFunc
+{
+    public static function randomStringLower(int $length = 10):string {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyz';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
+
+    public static function fixPersianUnicode($persianStr):string{
+        return preg_replace_callback('/\\\\u([0-9a-fA-F]{4})/', function ($match) {
+            return mb_convert_encoding(pack('H*', $match[1]), 'UTF-8', 'UTF-16BE');
+        }, $persianStr);
+    }
+}
