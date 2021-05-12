8f1565386ce883520c3b7a30fb3179be36429272
MrMohebi <MMMohebi@outlook.com>
2021-05-12 17:12:49 +0430

    add resOwner rout

--- routes/api/v1/api.php
+++ routes/api/v1/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\api\resOwner\v1\res;
 
 Route::prefix("/cuki")->group(function (){
 
@@ -12,7 +12,11 @@ Route::prefix("/res")->group(function (){
 
 });
 
-Route::prefix("/admin")->group(function (){
+Route::prefix("/resOwner")->group(function (){
+    Route::post("/createNewRes",[res::class,"createNewRes"]);
+});
+
+Route::prefix("admin")->group(function (){
 
 });
 
@@ -23,4 +27,3 @@ Route::prefix("/qr")->group(function (){
 Route::prefix("/pay")->group(function (){
 
 });
-
