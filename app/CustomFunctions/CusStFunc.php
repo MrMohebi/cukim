8d993f2cd4f8c312d60645b1766f4c04fd45a3f6
MrMohebi <MMMohebi@outlook.com>
2021-05-12 16:48:07 +0430

    add randomAlphabetStringLower

--- app/CustomFunctions/CusStFunc.php
+++ app/CustomFunctions/CusStFunc.php
@@ -16,6 +16,16 @@ class CusStFunc
         return $randomString;
     }
 
+    public static function randomAlphabetStringLower(int $length = 10):string {
+        $characters = 'abcdefghijklmnopqrstuvwxyz';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
+
     public static function fixPersianUnicode($persianStr):string{
         return preg_replace_callback('/\\\\u([0-9a-fA-F]{4})/', function ($match) {
             return mb_convert_encoding(pack('H*', $match[1]), 'UTF-8', 'UTF-16BE');
