9ba35f858eb4734aebf38111329af3d820504541
MrMohebi <MMMohebi@outlook.com>
2021-05-13 17:28:45 +0430

    add middleware res owner token checker

--- routes/api/v1/api.php
+++ routes/api/v1/api.php
@@ -3,7 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\api\resOwner\v1\res;
-use App\Http\Controllers\api\admin\v1\login;
+use App\Http\Controllers\api\resOwner\v1\signup;
 use App\Http\Controllers\createCategory;
 
 Route::prefix("/cuki")->group(function (){
@@ -11,12 +11,16 @@ Route::prefix("/cuki")->group(function (){
 });
 
 Route::prefix("/res")->group(function (){
-Route::post('createCategory',[createCategory::class,['createCategory']]);
-Route::post('createFood',[createCategory::class,['createCategory']]);
+    Route::post('createCategory',[createCategory::class,['createCategory']]);
+    Route::post('createFood',[createCategory::class,['createCategory']]);
 });
 
 Route::prefix("/resOwner")->group(function (){
-    Route::post("/createNewRes",[res::class,"createNewRes"]);
+    Route::group(["middleware"=>['resOwnerToken']], function (){
+        Route::post("/createNewRes",[res::class,"createNewRes"]);
+    });
+
+    Route::post("/signup",[signup::class,"signup"]);
 });
 
 Route::prefix("admin")->group(function (){
