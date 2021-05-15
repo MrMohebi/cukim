cb48b48950b2b4dd10dbf4c4a3d595557781cdce
MrMohebi <MMMohebi@outlook.com>
2021-05-15 18:28:03 +0430

    unset not required fields

--- app/Http/Controllers/api/resOwner/v1/signin.php
+++ app/Http/Controllers/api/resOwner/v1/signin.php
@@ -18,9 +18,14 @@ class signin extends Controller
         if($validator->fails())
             return response(["massage"=>$validator->errors()->all(), "statusCode"=>400],"400");
 
-        $resOwnerInfo = DB::table("res-owners")->where("username",$request->input("username"))->first();
+        $resOwnerInfo = DB::table("res_owners")->where("username",$request->input("username"))->first();
+
+
 
         if(password_verify($request->input("password"),$resOwnerInfo->password)){
+            unset($resOwnerInfo->password);
+            unset($resOwnerInfo->verification_code);
+            unset($resOwnerInfo->verification_code_tries);
             return response(["data"=>CusStFunc::arrayKeysToCamel(json_decode(json_encode($resOwnerInfo),true)), "statusCode"=>200],"200");
         }else{
             return response(["massage"=>"username or password are incorrect", "statusCode"=>401],"401");
