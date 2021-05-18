e9df474be49e47082a62b6d39409186810992910
MrMohebi <MMMohebi@outlook.com>
2021-05-18 19:09:16 +0430

    get english name by token

--- app/Http/Middleware/setResConn.php
+++ app/Http/Middleware/setResConn.php
@@ -6,6 +6,7 @@ use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\Facades\DB;
+use App\DatabaseNames\DN;
 
 class setResConn
 {
@@ -18,12 +19,19 @@ class setResConn
      */
     public function handle(Request $request, Closure $next)
     {
+        $resDatabaseName ="";
+        if($request->input("token")){
+            $resDatabaseName = DB::table(DN::tables["RESTAURANTS"])
+                ->where(DN::RESTAURANTS["token"], $request->input("token"))
+                ->value(DN::RESTAURANTS["DBName"]);
+        }
         if($request->input("resEnglishName")){
-            $resDatabaseName = DB::table("restaurants")
-                ->where("english_name", $request->input("resEnglishName"))
+            $resDatabaseName = DB::table(DN::tables["RESTAURANTS"])
+                ->where(DN::RESTAURANTS["eName"], $request->input("resEnglishName"))
                 ->first()
-                ->value("db_name");
-
+                ->value(DN::RESTAURANTS["DBName"]);
+        }
+        if(strlen($resDatabaseName) > 2){
             Config::set('database.connections.resConn.database', $resDatabaseName);
         }
         return $next($request);
