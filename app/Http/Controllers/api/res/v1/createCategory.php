f8e7041a95ccac12e95a06fd311c9f65d09e3880
MrMohebi <MMMohebi@outlook.com>
2021-05-18 19:10:37 +0430

    fix createCategory.php

--- app/Http/Controllers/api/res/v1/createCategory.php
+++ app/Http/Controllers/api/res/v1/createCategory.php
@@ -3,14 +3,18 @@
 namespace App\Http\Controllers\api\res\v1;
 
 use App\Http\Controllers\Controller;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use App\DatabaseNames\DN;
 
 class createCategory extends Controller
 {
     function createCategory(Request $request)
     {
 
-        $validatedData = Validator::make($request, [
+        $validatedData = Validator::make($request->all(), [
             "catPersianName" => 'min:2|required',
             "catEnglishName" => 'min:2|required',
         ]);
@@ -20,34 +24,35 @@ class createCategory extends Controller
 
 
             $catPersianName = $request->input('catPersianName');
-            $catEnglishName = $request->catEnglishName;
-            $logo = $request->logo;
-            $rank = $request->rank;
-            $type = $request->type;
-            $resEnglishName = $request->resEnglishName;
-            $averageColor = $request->averageColor;
+            $catEnglishName = $request->input('catEnglishName');
+            $logo = $request->input('logo') ?? "";
+            $rank = $request->input('type') ?? "";
+            $type = $request->input('type') ?? "";
+            $resEnglishName = $request->input('resEnglishName') ?? "";
+            $averageColor = $request->input('averageColor') ?? "";
 
 
             if (
-                DB::table('food_groups')->where('persian_name', $catPersianName)->exists() &&
-                DB::table('food_groups')->where('english_name', $catEnglishName)->exists()
+                DB::table(DN::tables["FOOD_GROUPS"])->where(DN::FOOD_GROUPS['pName'], $catPersianName)->exists() &&
+                DB::table(DN::tables["FOOD_GROUPS"])->where(DN::FOOD_GROUPS['eName'], $catEnglishName)->exists()
             ) {
                 return response(["message" => "some of info are duplicate", 'statusCode' => 402], 402);
             }
 
             $insertCreateCat = [
-                "persian_name" => $catPersianName,
-                "english_name" => $catEnglishName,
-                "logo" => $logo,
-                "status" => "active",
-                "type" => $type,
-                "res_english_name" => $resEnglishName,
-                "average_color" => $averageColor
+                DN::FOOD_GROUPS['pName'] => $catPersianName,
+                DN::FOOD_GROUPS['eName'] => $catEnglishName,
+                DN::FOOD_GROUPS['logo'] => $logo,
+                DN::FOOD_GROUPS['status'] => "active",
+                DN::FOOD_GROUPS['type'] => $type,
+                DN::FOOD_GROUPS['resEName'] => $resEnglishName,
+                DN::FOOD_GROUPS['averageColor'] => $averageColor,
+                DN::CA=> Carbon::now()->timestamp,
             ];
 
 
-            if (DB::table('food_groups')->insert($insertCreateCat)) {
-                return response(['statusCode' => 200], 200);
+            if (DB::table(DN::tables["FOOD_GROUPS"])->insert($insertCreateCat)) {
+                return response(['statusCode' => 200]);
 
             } else {
                 return response(["message" => "some thing went wrong during creating new category on server", 'statusCode' => 500], 500);
